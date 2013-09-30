0ebc59510496f65ee1f6765efdee78dee2389b47
Cat Johnson <cfjohnson@earthlink.net>
2013-09-30 13:02:47 -0600

    developed infinite slider with interval timer.

---
+++ js/infiniteSlider.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	var speed = 5000,
+		run = setInterval("rotate()", speed),
+		item_width = $("#slides li").outerWidth(),
+		left_value = item_width * (-1);
+
+	$("slides li:first").before($("#slides li:last"));
+	$("#slides ul").css({"left": left_value});
+
+	$("#prev").click(function(){
+		var left_indent = parseInt($("#slides ul").css("left")) + item_width;
+
+		$("#slides ul").animate({"left": left_indent}, 200, function(){
+			$("#slides li:first").before($("#slides li:last"));
+			$("#slides ul").css({"left": left_value});
+		});
+		return false;
+	});	
+
+	$("#next").click(function(){
+		var left_indent = parseInt($("#slides ul").css("left")) - item_width;
+
+		$("#slides ul").animate({"left": left_indent}, 200, function(){
+			$("#slides li:last").after($("#slides li:first"));
+			$("#slides ul").css({"left": left_value});
+		});
+		return false;
+	});
+
+	$("#slides").hover(
+		function() {
+			clearInterval(run);
+		},
+		function() {
+			run = setInterval("rotate()", speed);
+		}
+	);
+});
+
+function rotate() {
+	$("#next").click();
+}
+
