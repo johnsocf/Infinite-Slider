b2ad4179fd5921f5fb422a24c742f12cf83859d1
Cat Johnson <cfjohnson@earthlink.net>
2013-09-30 22:50:12 -0600

    finished most of slider - working on pagination and subsequent animation.

--- js/infiniteSlider.js
+++ js/infiniteSlider.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
 	var speed = 5000,
 		run = setInterval("rotate()", speed),
 		item_width = $("#slides li").outerWidth(),
-		left_value = item_width * (-1);
+		left_value = item_width * (-1),
+		left_indent = (parseInt($("#slides ul").css("left")) + item_width),
+		distanceFromStart = 0,
+		listItems = $("#slides ul").children();
+		length = listItems.length;
+		counter = 0;
 
 	$("slides li:first").before($("#slides li:last"));
 	$("#slides ul").css({"left": left_value});
@@ -13,16 +18,31 @@ $(document).ready(function(){
 		$("#slides ul").animate({"left": left_indent}, 200, function(){
 			$("#slides li:first").before($("#slides li:last"));
 			$("#slides ul").css({"left": left_value});
+			distanceFromStart = distanceFromStart - 250;
+			console.log("distance from start: " + distanceFromStart);
+			if (counter >= -Math.abs(length-2)) {
+				counter --;
+			}
+			else {counter = 0;}
+			console.log("counter: " + counter);
+			console.log("list: " + length);
 		});
 		return false;
 	});	
 
-	$("#next").click(function(){
+	$("#next").click(function(left_indent){
 		var left_indent = parseInt($("#slides ul").css("left")) - item_width;
-
 		$("#slides ul").animate({"left": left_indent}, 200, function(){
 			$("#slides li:last").after($("#slides li:first"));
 			$("#slides ul").css({"left": left_value});
+			distanceFromStart = 250 + distanceFromStart;
+			if (counter <= (length-2)) {
+				counter ++;
+			}
+			else {counter = 0;}
+			console.log("distance from start: " + distanceFromStart);
+			console.log("counter: " + counter);
+			console.log("list: " + length);
 		});
 		return false;
 	});
@@ -35,9 +55,39 @@ $(document).ready(function(){
 			run = setInterval("rotate()", speed);
 		}
 	);
+
+	$(".pagination-list-item a").click(function(event){
+		var number = $(this).text(),
+			placeInList = parseInt(number) + 1;
+		placeInList = parseInt(number) + 1;
+		distanceFromStart = left_indent * placeInList;
+		console.log(distanceFromStart);
+		console.log(placeInList);
+		console.log(left_indent);
+		$("#next").click(left_indent);
+	});
+
 });
 
+function paginate () {
+	listItems = $("#slides ul").children();
+	length = listItems.length;
+	console.log("length: " + length);
+	$("#pagination").css("width", "(" + listItems + ")");
+	for (var i = 0; i < listItems.length; i++) {
+		var oneWidth = listItems.size(),
+			totalWidth = oneWidth * 26;
+		$("#pagination").append("<li><a>" + i + "</a></li>");
+		console.log(i);
+	}
+	$("#pagination li").addClass("pagination-list-item")
+	$("#pagination").css("width", totalWidth);
+}
+paginate();
+
 function rotate() {
-	$("#next").click();
+	var left_indent = parseInt($("#slides ul").css("left")) + item_width,
+		item_width = $("#slides li").outerWidth();
+	// $("#next").click();
 }
 
